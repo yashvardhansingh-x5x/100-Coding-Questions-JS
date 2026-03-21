70e5bca41ff9912b47f03dda30104bbf3ab96360
yashvardhansingh-x5x <singhyashvardhan582@gmail.com>
2026-03-21 10:01:04 +0530

    Refactor array and object utility functions; add new functionalities for sorting, merging, and finding duplicates

--- ARRAYS.js
+++ ARRAYS.js
@@ -1,55 +1,174 @@
 // Array ka max element - Done
-
+function maxElement(arr){
+    return Math.max(...arr)
+}
 
 // Array ka min element - Done
-
+function maxElement(arr){
+    return Math.min(...arr)
+}
 
 // Sum of array elements - Done
-
+function sumArray(arr) {
+  return arr.reduce((sum, num) => sum + num, 0);
+}
 
 // Average of array - Done
-
+function Average(arr) {
+    retun sunArray(arr) / arr.length;
+}
 
 // Remove duplicates - Done
-
+function removeDuplicates(arr){
+    retun[...new Set(arr)];
+}
 
 // Second largest element - Done
-
+function secondLargest(arr) {
+  let unique = [...new Set(arr)];
+  unique.sort((a, b) => b - a);
+  return unique[1];
+}
 
 // Array reverse - Done
-
+function reverseArray(arr){
+    return arr.reverse();
+}
 
 // Array rotate left - Done
+function rotateLeft(arr, k) {
+  k = k % arr.length;
 
+  return arr.slice(k).concat(arr.slice(0, k));
+}
+
+console.log(rotateLeft([1,2,3,4,5], 2));
 
 // Array flatten
+function flattenArray(arr) {
+  let result = [];
 
+  for (let item of arr) {
+    if (Array.isArray(item)) {
+      result = result.concat(flattenArray(item));
+    } else {
+      result.push(item);
+    }
+  }
+
+  return result;
+}
+
+console.log(flattenArray([1, [2, [3, 4]], 5]));
 
 // Frequency count
+function frequencyCount(arr) {
+  let freq = {};
 
+  for (let num of arr) {
+    freq[num] = (freq[num] || 0) + 1;
+  }
+
+  return freq;
+}
+
+console.log(frequencyCount([1,2,2,3,3,3]));
 
 // Check array sorted hai ya nahi
+function isSorted(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] > arr[i + 1]) {
+      return false;
+    }
+  }
+  return true;
+}
 
+console.log(isSorted([1,2,3]));
 
 // Merge two arrays - Done
-
+function mergeArrays(arr1, arr2) {
+  return [...arr1, ...arr2];
+}
 
 // Intersection of arrays
+function intersection(arr1, arr2) {
+  let result = [];
 
+  for (let val of arr1) {
+    if (arr2.includes(val)) {
+      result.push(val);
+    }
+  }
+
+  return result;
+}
+
+console.log(intersection([1,2,3], [2,3,4]));
 
 // Difference of arrays
+function difference(arr1, arr2) {
+  let result = [];
 
+  for (let val of arr1) {
+    if (!arr2.includes(val)) {
+      result.push(val);
+    }
+  }
+
+  return result;
+}
+
+console.log(difference([1,2,3], [2,3,4]));
 
 // Remove falsy values
+function removeFalsy(arr) {
+  let result = [];
 
+  for (let val of arr) {
+    if (val) {
+      result.push(val);
+    }
+  }
+
+  return result;
+}
+
+console.log(removeFalsy([0,1,false,2,"",3]));
 
 // Chunk array
+function chunkArray(arr, size) {
+  let result = [];
 
+  for (let i = 0; i < arr.length; i += size) {
+    let chunk = arr.slice(i, i + size);
+    result.push(chunk);
+  }
+
+  return result;
+}
+
+console.log(chunkArray([1,2,3,4,5], 2));
 
 // Find missing number
-
+function findMissing(arr, n) {
+  let expected = (n * (n + 1)) / 2;
+  let actual = arr.reduce((sum, num) => sum + num, 0);
+  return expected - actual;
+}
 
 // Count even & odd elements
-
+function countEvenOdd(arr) {
+  let even = 0, odd = 0;
+  for (let num of arr) {
+    num % 2 === 0 ? even++ : odd++;
+  }
+  return { even, odd };
+}
 
 // Move all zeros to end
+function moveZeros(arr) {
+  let nonZero = arr.filter(n => n !== 0);
+  let zeros = arr.filter(n => n === 0);
+  return [...nonZero, ...zeros];
+}
--- OBJECTS.js
+++ OBJECTS.js
@@ -1,28 +1,88 @@
 // Object me key exist karti hai ya nahi - Done
+let obj = { name: "Yash", age: 20 };
 
+let key = "age";
+
+if (obj.hasOwnProperty(key)) {
+    console.log("Key exists");
+} else {
+    console.log("Key does not exist");
+}
 
 // Object ke keys print karo - Done
+let obj = { name: "Yash", age: 20 };
 
+let keys = Object.keys(obj);
+console.log(keys);
 
 // Object ke values print karo - Done
+let obj = { name: "Yash", age: 20 };
 
+let values = Object.values(obj);
+console.log(values);
 
 // Object ko array me convert karo - Done
+let obj = { name: "Yash", age: 20 };
 
+let arr = Object.entries(obj);
+console.log(arr);
 
 // Array ko object me convert karo - Done
+let arr = [["name", "Yash"], ["age", 20]];
 
+let obj = Object.fromEntries(arr);
+console.log(obj);
 
 // Merge two objects - Done
+let obj1 = { a: 1, b: 2 };
+let obj2 = { c: 3, d: 4 };
 
+let merged = { ...obj1, ...obj2 };
+
+console.log(merged);
 
 // Deep clone object - Done
+let obj = { name: "Yash", address: { city: "Delhi" } };
 
+let clone = JSON.parse(JSON.stringify(obj));
+
+clone.address.city = "Noida";
+
+console.log(obj);    // original safe
+console.log(clone);  // changed
 
 // Object ko sort karo by value - DOne
+let obj = { a: 3, b: 1, c: 2 };
 
+let sorted = Object.fromEntries(
+    Object.entries(obj).sort((a, b) => a[1] - b[1])
+);
+
+console.log(sorted);
 
 // Nested object se value nikaalo - DOne
+let obj = {
+    user: {
+        profile: {
+            name: "Yash"
+        }
+    }
+};
 
+console.log(obj.user.profile.name);
 
 // Object me duplicate values find karo - Done
+let obj = { a: 1, b: 2, c: 1, d: 3, e: 2 };
+
+let values = Object.values(obj);
+let duplicates = [];
+
+for (let i = 0; i < values.length; i++) {
+    for (let j = i + 1; j < values.length; j++) {
+        if (values[i] === values[j] && !duplicates.includes(values[i])) {
+            duplicates.push(values[i]);
+        }
+    }
+}
+
+console.log(duplicates);
