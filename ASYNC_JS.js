947e114a6d9252b4d3ca701d954f3b948c0a0ed9
yashvardhansingh-x5x <singhyashvardhan582@gmail.com>
2026-03-21 16:45:51 +0530

    Update ASYNC_JS.js

--- ASYNC_JS.js
+++ ASYNC_JS.js
@@ -1,13 +1,103 @@
 // Callback example - DOne
+function greet(name, callback) {
+  console.log("Hello " + name);
+  callback();
+}
 
+function sayBye() {
+  console.log("Goodbye!");
+}
+
+greet("Yash", sayBye);
 
 // Callback hell example - Done
+setTimeout(() => {
+  console.log("Step 1");
 
+  setTimeout(() => {
+    console.log("Step 2");
+
+    setTimeout(() => {
+      console.log("Step 3");
+
+      setTimeout(() => {
+        console.log("Step 4");
+      }, 1000);
+
+    }, 1000);
+
+  }, 1000);
+
+}, 1000);
 
 // Promise create karo - DOne
+const myPromise = new Promise((resolve, reject) => {
+  let success = true;
 
+  if (success) {
+    resolve("Task completed");
+  } else {
+    reject("Task failed");
+  }
+});
+
+myPromise
+  .then(res => console.log(res))
+  .catch(err => console.log(err));
 
 // Promise chaining - Done
+function step1() {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      console.log("Step 1");
+      resolve();
+    }, 1000);
+  });
+}
 
+function step2() {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      console.log("Step 2");
+      resolve();
+    }, 1000);
+  });
+}
+
+function step3() {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      console.log("Step 3");
+      resolve();
+    }, 1000);
+  });
+}
+
+step1()
+  .then(step2)
+  .then(step3)
+  .then(() => console.log("All Done"));
 
 // async/await example - Done
+function delay(msg) {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(msg);
+    }, 1000);
+  });
+}
+
+async function run() {
+  let res1 = await delay("Step 1");
+  console.log(res1);
+
+  let res2 = await delay("Step 2");
+  console.log(res2);
+
+  let res3 = await delay("Step 3");
+  console.log(res3);
+
+  console.log("All Done");
+}
+
+run();
